2665135a507644adc2f1f8c920331d7bc6ec00e4
Sandesh Deshmukh <sandeshdeshmukh2704@gmail.com>
2025-02-02 15:17:29 +0530

    dexstop view complete

--- src/sections/home/Help.jsx
+++ src/sections/home/Help.jsx
@@ -3,49 +3,72 @@
 
 import PaddingX from '@/components/PaddingX';
 import SectionTitle from '@/components/SectionTitle';
-import { useState } from 'react';
-import { RxArrowTopRight } from "react-icons/rx";
+import Image from 'next/image';
+import { TbTarget } from "react-icons/tb";
+import { LuBookOpen } from "react-icons/lu";
+import { CiCalendarDate } from "react-icons/ci";
+import { VscGraph } from "react-icons/vsc";
+import Paragraph from '@/components/Paragraph';
 
 export default function Help() {
-    const [selectedQuestion, setSelectedQuestion] = useState(1);
-
-    const faqs = [
+    const services = [
         {
-            id: 1,
-            question: "How do I find the right tutor for my child?",
-            answer: "You can browse through our tutor profiles and filter by subject expertise, experience level, and availability to find the perfect match for your child's needs."
+            title: "Understanding Your Goals",
+            description: "We begin by learning about your child's unique learning requirements, academic goals, and preferred teaching style to ensure a customized tutoring experience",
+            icon: <TbTarget />,
+            imageUrl: "/assests/help/07446903b07a56a8f431d15375c66be3.png",
+            isReversed: false
         },
         {
-            id: 2,
-            question: "Are the tutors qualified and experienced?",
-            answer: "Yes, all our tutors are thoroughly vetted, professionally trained, and have proven expertise in their respective fields."
+            title: "Finding the Perfect Tutor",
+            description: "Based on your preferences, we pair your child with an experienced tutor who specializes in the subject and grade level, ensuring the perfect match.",
+            icon: <LuBookOpen />,
+            imageUrl: "/assests/help/8cf5a92713f114f959795a7de6b73dca.png",
+            isReversed: true
         },
         {
-            id: 3,
-            question: "Can I choose between online and in-person tutoring?",
-            answer: "Yes, we offer both online and in-person tutoring options to accommodate your preferences and schedule."
+            title: "Personalized Learning Sessions",
+            description: "Our tutors deliver tailored lessons, focusing on building understanding and confidence while addressing key areas for improvement.",
+            icon: <CiCalendarDate />,
+            imageUrl: "/assests/help/ca7b69c39f5863c6d90999b27501e9f4.png",
+            isReversed: false
         },
         {
-            id: 4,
-            question: "What subjects and grade levels do you cover?",
-            answer: "We cover a wide range of subjects across all grade levels, from elementary to high school and beyond."
-        },
-        {
-            id: 5,
-            question: "How flexible are the schedules?",
-            answer: "Our tutoring schedules are highly flexible, allowing you to book sessions at times that work best for you and your child."
+            title: "Tracking Progress & Feedback",
+            description: "We provide regular progress updates and encourage open communication to ensure your child is thriving and achieving their academic goals.",
+            icon: <VscGraph />,
+            imageUrl: "/assests/help/07446903b07a56a8f431d15375c66be3.png",
+            isReversed: true
         }
     ];
 
     return (
         <div className="bg-white py-16">
             <PaddingX>
-     
-                    <div className='flex justify-center w-full'>
-                        <SectionTitle title="How we can help you succeed" black={true}/>
-                    </div>
+
+                <div className='flex justify-center w-full'>
+                    <SectionTitle title="How we can help you succeed" black={true} />
+                </div>
+                <div className="space-y-7 w-[85%] mx-auto  mt-12">
+                    {services.map((item, index) => (
+                        <div className={`h-full grid grid-cols-12`} key={index}>
+                            <div className={`col-span-5 bg-[#B4EAE4] h-52 w-full p-8 rounded-xl  ${index % 2 !== 0 ? 'order-first ' : 'order-last'} `}>
+                                <h2 className="text-2xl font-bold mb-4">{item.title}</h2>
+                                <Paragraph text={item.description} />
+                            </div>
+                            <div className='col-span-2 px-10 flex flex-col justify-center items-center'>
+                                <div className='text-3xl text-white bg-[#011627] p-4 rounded-full '>
+                                    {item.icon}
+                                </div>
+                                <div className="flex-1 border-l-2 border-dashed border-[#011627]/50" />
+                            </div>
+                            <div className={`col-span-5 w-full ${index % 2 !== 0 ? 'order-last ' : 'order-first'}`}>
+                                <Image src={item.imageUrl} alt={item.title} width={500} height={500} className="h-52 w-full  object-cover  rounded-xl " />
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </PaddingX>
         </div>
     );
 };
-
--- src/sections/home/Testimonial.jsx
+++ src/sections/home/Testimonial.jsx
@@ -1,125 +1,3 @@
-
-// "use client"
-// import PaddingX from '@/components/PaddingX';
-// import SectionTitle from '@/components/SectionTitle'
-// import Image from 'next/image';
-// import React, { useState } from 'react'
-// import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-// import { FaStar } from "react-icons/fa6";
-
-
-// export default function Testimonial() {
-//   const testimonials = [
-//     {
-//       name: 'Joshua Enith',
-//       role: 'Student',
-//       image: '/assests/testmonials/b08ed33d47db4b9049c21f94a74a01ab.jpg',
-//       rating: 5,
-//       text: 'Finding the right tutor for my child was such a relief. The personalized sessions have improved not only their grades but also their confidence. Highly recommended!'
-//     },
-//     {
-//       name: 'Sarah Chen',
-//       role: 'Parent',
-//       image: '/assests/testmonials/b08ed33d47db4b9049c21f94a74a01ab.jpg',
-//       rating: 5,
-//       text: 'The tutoring platform has been a game-changer for my daughter. The quality of instruction is outstanding.'
-//     },
-//     {
-//       name: 'Michael Roberts',
-//       role: 'Student',
-//       image: '/assests/testmonials/b08ed33d47db4b9049c21f94a74a01ab.jpg',
-//       rating: 5,
-//       text: 'I appreciate how flexible and accommodating the tutors are. My grades have improved significantly.'
-//     }
-//   ];
-
-//   const [currentSlide, setCurrentSlide] = useState(0);
-
-//   const nextSlide = () => {
-//     setCurrentSlide((prev) => (prev + 1) % testimonials.length);
-//   };
-
-//   const prevSlide = () => {
-//     setCurrentSlide((prev) => (prev - 1 + testimonials.length) % testimonials.length);
-//   };
-
-
-
-//   return (
-//     <>
-//       <section className="bg-black text-white py-16">
-//         <PaddingX>
-//           <div className="flex justify-center">
-//             <div className="w-[40rem] text-center">
-//               <SectionTitle title="Discover what our students say about us" balance={true} />
-//             </div>
-//           </div>
-
-
-//           <div className="flex justify-center bg-cover bg-center" style={{backgroundImage:  "url('/assests/testmonials/9a964c330c0bb6912707d1d23495af18.png')"}}>
-        
-//               <div className="relative ">
-//                 <button
-//                   onClick={prevSlide}
-//                   className="absolute left-4 top-1/2 -translate-y-1/2 p-2 rounded-full bg-gray-800 hover:bg-gray-700 transition-colors"
-//                 >
-//                   <FaChevronLeft className="w-6 h-6 text-white" />
-//                 </button>
-
-//                 <div className="">
-//                   <div className="flex flex-col md:flex-row items-center gap-8">
-//                     <div className=" relative py-7 px-2 rounded-2xl border-2  -rotate-6 border-[#2EC4B6]">
-//                       <Image
-//                         height={400}
-//                         width={200}
-//                         priority
-//                         src={testimonials[currentSlide].image}
-//                         alt={testimonials[currentSlide].name}
-//                         className="rounded-lg object-cover w-full h-full"
-//                       />
-
-//                       <div className='flex text-center flex-col mt-4'>
-//                         <h3 className="text-white text-xl font-semibold">
-//                           {testimonials[currentSlide].name}
-//                         </h3>
-//                         <span className="text-gray-400 text-base mt-1">
-//                           {testimonials[currentSlide].role}
-//                         </span>
-
-//                         <div className="flex items-center gap-1 mt-2 justify-center">
-//                           {[...Array(testimonials[currentSlide].rating)].map((_, i) => (
-//                             <span className='text-yellow-400 text-lg'> <FaStar/></span>
-//                           ))}
-//                         </div>
-//                       </div>
-
-//                     </div>
-
-//                     <div className="flex-1 w-40 text-center md:text-left">
-//                       <p className="text-white text-lg mb-4">
-//                         {testimonials[currentSlide].text}
-//                       </p>
-
-//                     </div>
-//                   </div>
-//                 </div>
-
-//                 <button
-//                   onClick={nextSlide}
-//                   className="absolute right-4 top-1/2 -translate-y-1/2 p-2 rounded-full bg-gray-800 hover:bg-gray-700 transition-colors"
-//                 >
-//                   <FaChevronRight className="w-6 h-6 text-white" />
-//                 </button>
-//               </div>
-//             </div>
-
-//         </PaddingX>
-//       </section>
-//     </>
-//   )
-// }
-
-
 'use client'
 
 import Image from 'next/image'
@@ -132,7 +10,8 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 
 const testimonials = [
-  {
+   {
+     id:1,
     name: 'Joshua Enith',
     role: 'Student',
     image: '/assests/testmonials/b08ed33d47db4b9049c21f94a74a01ab.jpg',
@@ -140,13 +19,15 @@ const testimonials = [
     rating: 5
   },
   {
+    id:2,
     name: 'Joshua Enith',
     role: 'Student',
     image: '/assests/testmonials/b08ed33d47db4b9049c21f94a74a01ab.jpg',
     quote: 'Finding the right tutor for my child was such a relief. The personalized sessions have improved not only their grades but also their confidence. Highly recommended!',
-     rating: 5
+    rating: 5
   },
   {
+    id:3,
     name: 'Joshua Enith',
     role: 'Student',
     image: '/assests/testmonials/b08ed33d47db4b9049c21f94a74a01ab.jpg',
@@ -208,8 +89,8 @@ export default function TestimonialSlider() {
 
     <div className="max-w-5xl mx-auto mt-10"  >
       <Slider {...settings}>
-        {testimonials.map((testimonial) => (
-          <div key={testimonial.name} className="px-20 py-5">
+        {testimonials.map((testimonial, index) => (
+          <div key={testimonial.id} className="px-20 py-5">
             <div className="flex flex-col md:flex-row items-center gap-12">
               <div className=" relative py-7 px-3 rounded-2xl border-2  -rotate-6 border-[#2EC4B6]">
                        <Image
@@ -231,7 +112,7 @@ export default function TestimonialSlider() {
 
                         <div className="flex items-center gap-1 mt-2 justify-center">
                           {[...Array(testimonial.rating)].map((_, i) => (
-                            <span className='text-yellow-400 text-lg'> <FaStar/></span>
+                            <span className='text-yellow-400 text-lg' key={i}> <FaStar/></span>
                           ))}
                         </div>
                       </div>
